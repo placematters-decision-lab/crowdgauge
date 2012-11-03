4a6df586331452b268e0790d9fc25bdfdd14d9d7
Ken Goulding <gouldingken@gmail.com>
2012-11-02 23:14:11 -0400

    play button

--- client/contribute/contrib-main.js
+++ client/contribute/contrib-main.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function () {
 //    var socket = io.connect('http://localhost');
 
     var socket = io.connect();
@@ -11,13 +11,17 @@ $(document).ready(function() {
 
     var matrix = new SAS.MatrixUI($("#holder"), socket);
     matrix.createUI();
-    matrix.onTabChanged(function(isActions) {
-        $( "#instructions" ).accordion( "option", "active", (isActions) ? 1 : 0 );
+    matrix.onTabChanged(function (isActions) {
+        $("#instructions").accordion("option", "active", (isActions) ? 1 : 0);
     });
 
-    $( "#instructions" ).accordion({
-        heightStyle: "content",
-        collapsible: true
+    $("#instructions").accordion({
+        heightStyle:"content",
+        collapsible:true
+    });
+
+    $('#btnPlay').button().click(function() {
+        document.location.href = '../play/index.html';
     });
 
 });
