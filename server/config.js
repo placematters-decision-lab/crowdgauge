f3943634629b4755b0f6639e5085741da2aee24c
Jason Lally <jason.lally@gmail.com>
2013-03-17 18:39:11 -0600

    simplify dotcloud config

--- server/config.js
+++ server/config.js
@@ -4,38 +4,17 @@
  * If you're using DotCloud you can leave this as is unless you want to use a custom URL.
  */
 var fs = require('fs');
-
-fs.exists('/home/dotcloud/environment.json', function(exists) {
-    if (exists) {
-        var env = JSON.parse(fs.readFileSync('/home/dotcloud/environment.json', 'utf-8'));
-        module.exports.appURL = env['DOTCLOUD_WWW_HTTP_URL'];
-        module.exports.couchURL = env['COUCH_URL'];
-        module.exports.verbosity = 0;
-        module.exports.redis = {
-            host:env['DOTCLOUD_DATA_REDIS_HOST'],
-            port:env['DOTCLOUD_DATA_REDIS_PORT'], //9859 = default redis port, use 6379 for iris
-            key:env['DOTCLOUD_DATA_REDIS_PASSWORD']
-        };
-        module.exports.loggly = null;
+var env = JSON.parse(fs.readFileSync('/home/dotcloud/environment.json', 'utf-8'));
+module.exports.appURL = env['DOTCLOUD_WWW_HTTP_URL'];
+module.exports.couchURL = env['COUCH_URL'];
+module.exports.verbosity = 0;
+module.exports.redis = {
+    host: env['DOTCLOUD_DATA_REDIS_HOST'],
+    port: env['DOTCLOUD_DATA_REDIS_PORT'], //9859 = default redis port, use 6379 for iris
+    key: env['DOTCLOUD_DATA_REDIS_PASSWORD']
+};
+module.exports.loggly = null;
 //module.exports.loggly = {
 //    conf:{ subdomain:'yourdomain' },
 //    key:'your-key'
 //};
-
-    } else {
-        module.exports.appURL = (process.env.NODE_ENV == 'production') ? 'http://'+process.env.SUBDOMAIN+'.jit.su' : 'http://localhost:8080';
-        module.exports.couchURL = process.env.COUCH_URL;
-        module.exports.verbosity = 0;
-        module.exports.redis = {
-            host:process.env.REDIS_HOST,
-            port:process.env.REDIS_PORT, //9859 = default redis port, use 6379 for iris
-            key:process.env.REDIS_KEY
-        };
-        module.exports.loggly = null;
-//module.exports.loggly = {
-//    conf:{ subdomain:'yourdomain' },
-//    key:'your-key'
-//};
-    }
-});
-
