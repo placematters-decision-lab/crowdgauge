0c67751e3c10990c2f12b95caf8099fc4286b501
Jason Lally <jason.lally@gmail.com>
2013-04-08 12:40:56 -0600

    change config

--- server/config.js
+++ server/config.js
@@ -1,21 +1,44 @@
 /**
  * @type {String} appURL should match your browser's address bar.
- * For debugging it may be helpful to provide a localhost URL.
- * If you're using DotCloud you can leave this as is unless you want to use a custom URL.
+ * For debugging it may be helpful to provide a localhost URL
+ * This config works in a mixed environment with dotcloud or local Node, also should work with NodeJitsu
+ * If you're using NodeJitsu then you can leave this as is unless you want to use a custom URL.
  */
-var fs = require('fs');
-var env = JSON.parse(fs.readFileSync('/home/dotcloud/environment.json', 'utf-8'));
-module.exports.appURL = env['DOTCLOUD_WWW_HTTP_URL'];
-module.exports.couchURL = env['COUCH_URL'];
-module.exports.verbosity = 0;
-module.exports.redis = {
-    host: env['DOTCLOUD_DATA_REDIS_HOST'],
-    port: env['DOTCLOUD_DATA_REDIS_PORT'], //9859 = default redis port, use 6379 for iris
-    key: env['DOTCLOUD_DATA_REDIS_PASSWORD']
-};
-module.exports.loggly = null;
-//module.exports.loggly = {
-//    conf:{ subdomain:'yourdomain' },
-//    key:'your-key'
-//};
 
+var config_couchURL = process.env.COUCH_URL;
+var config_appURL = (process.env.NODE_ENV == 'production') ? 'http://'+process.env.SUBDOMAIN+'.jit.su' : 'http://localhost:8080';
+var config_verbosity = 0;
+var config_redis_host = process.env.REDIS_HOST;
+var config_redis_port = process.env.REDIS_PORT;
+var config_redis_key = process.env.REDIS_KEY;
+var config_loggly = null;
+
+
+var fs = require('fs');
+fs.readFileSync('/home/dotcloud/environment.json', 'utf8', function (err,data) {
+    if (err) {
+        return console.log(err);
+    } else {
+        console.log(data);
+        env = JSON.parse(data);
+        config_appURL = env['DOTCLOUD_WWW_HTTP_URL'];
+        config_couchURL = env['COUCH_URL'];
+        config_redis_host = env['DOTCLOUD_DATA_REDIS_HOST'];
+        config_redis_port = env['DOTCLOUD_DATA_REDIS_PORT'];
+        config_redis_key = env['DOTCLOUD_DATA_REDIS_PASSWORD'];
+    }
+});
+/*
+ separated out config variables to make it easier to specify mixed use cases, defaults defined for a native node installation
+ */
+module.exports = {
+    appURL: config_appURL,
+    couchURL: config_couchURL,
+    verbosity: config_verbosity,
+    redis: {
+        host: config_redis_host,
+        port: config_redis_port, //9859 = default redis port, use 6379 for iris
+        key: config_redis_key
+    },
+    loggly: config_loggly
+}
