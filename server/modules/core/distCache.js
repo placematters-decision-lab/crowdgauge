9699b7aa0bae17e1d75c63f8cbc2d9a5f6bc9714
unknown <ycui@YCUI.sasaki.local>
2013-05-22 16:53:30 -0400

    1

--- server/modules/core/distCache.js
+++ server/modules/core/distCache.js
@@ -117,12 +117,13 @@ DistCache = function (type, onReady) {
             console.log("ENTERING MEMCACHE... onReady:"+_onReady);
             var mc = require('mc');
             _memCli = new mc.Client('sasakicache.s95c4z.cfg.use1.cache.amazonaws.com', mc.Adapter.json);//:11211
-            _memCli.connect(console.log("Connected to memcache1"), function () {
+            _memCli.connect(function () {
                 console.log("Connected to memcache");
                 if (_onReady) _onReady();
             });
 
             _get = function (key, callback) {
+                if (_onReady) _onReady();
                 console.log("INSIDE GET MEMCACHE...");
                 _memCli.get(key, function (err, response) {
                         if (!err) {
