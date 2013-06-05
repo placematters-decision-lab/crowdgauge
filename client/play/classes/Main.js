505e2d5a83b203e9b98e1ebe6ca88c9164f18fab
ycui <ycui@sasaki.com>
2013-06-05 11:54:47 -0400

    introTxt font-size auto update

--- client/play/classes/Main.js
+++ client/play/classes/Main.js
@@ -94,8 +94,9 @@
         };
 
         var _resizeMe = function () {
-            var fontsize_introTxt = 12;  //TODO: initial font
             var fontsize_introTxtH1 = 50;
+            var fontsize_introTxt = 12;  //TODO: initial font
+
 
             var preferredHeight = 768;
             var displayHeight = $(window).width();
@@ -103,8 +104,10 @@
             var newFontSize_introTxt = Math.floor(fontsize_introTxt * percentage) - 1;
             var newFontSize_introTxtH1 = Math.floor(fontsize_introTxtH1 * percentage) - 1;
 
-            $(".introTxt").css("font-size", newFontSize_introTxt);
             $(".introTxtH1").css("font-size", newFontSize_introTxtH1);
+            if (newFontSize_introTxt > 10) {
+                $(".introTxt").css("font-size", newFontSize_introTxt);
+            }
         };
         //endregion
 
