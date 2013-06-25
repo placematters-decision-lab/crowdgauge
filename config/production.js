dfe8e738522d3ec224a8439902c527d88784c481
Jason Lally <jason.lally@gmail.com>
2013-06-24 19:02:22 -0700

    include node-config dependency, add config files for default and production

---
+++ config/production.js
@@ -0,0 +1,19 @@
+/**
+ * @type {String} appURL should match your browser's address bar.
+ * For debugging it may be helpful to provide a localhost URL.
+ * If you're using DotCloud you can leave this as is unless you want to use a custom URL.
+ */
+var fs  = require('fs');
+var env = JSON.parse(fs.readFileSync('../environment.json', 'utf-8'));
+
+module.exports = {
+    appURUL : env['DOTCLOUD_WWW_HTTP_URL'],
+    couchURL : env['COUCH_URL'],
+    verbosity : 0,
+    redis : {
+        host : env['DOTCLOUD_DATA_REDIS_HOST'],
+        port : env['DOTCLOUD_DATA_REDIS_PORT'],
+        key : env['DOTCLOUD_DATA_REDIS_PASSWORD']
+    },
+    loggly : null
+}
